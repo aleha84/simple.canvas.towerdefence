73a737d573c62ad8688e807d3ebec1f9a2b697b8
Starykh Aleksey <astarikh@mail.ru>
2016-03-17 12:20:12 +0300

    performance test

--- js/go/placeable.js
+++ js/go/placeable.js
@@ -4,41 +4,41 @@ SCG.Placeable = {
 	enemyUnits: {},
 	show: false,
 	set: function(go){
-		var topLeft = new Vector2(Math.round(go.position.x - go.size.x/2), Math.round(go.position.y - go.size.y/2));
-		for(var x = 0; x<go.size.x;x++){
-			for(var y=0;y<go.size.y;y++){
-				var index = (topLeft.x+x) + 'x' + (topLeft.y+y); //String.format("{0}_{1}", topLeft.x+x, topLeft.y+y);
-				if(this.battlefield[index] == undefined) {
-					this.battlefield[index] = {};
-				}
+		// var topLeft = new Vector2(Math.round(go.position.x - go.size.x/2), Math.round(go.position.y - go.size.y/2));
+		// for(var x = 0; x<go.size.x;x++){
+		// 	for(var y=0;y<go.size.y;y++){
+		// 		var index = (topLeft.x+x) + 'x' + (topLeft.y+y); //String.format("{0}_{1}", topLeft.x+x, topLeft.y+y);
+		// 		if(this.battlefield[index] == undefined) {
+		// 			this.battlefield[index] = {};
+		// 		}
 
-				if(this.battlefield[index][go.id] == undefined){
-					this.battlefield[index][go.id] = go;
-				}
-			}
-		}
+		// 		if(this.battlefield[index][go.id] == undefined){
+		// 			this.battlefield[index][go.id] = go;
+		// 		}
+		// 	}
+		// }
 	},
 	remove: function(go){
-		var topLeft = new Vector2(Math.round(go.position.x - go.size.x/2), Math.round(go.position.y - go.size.y/2));
-		var controlSum = go.size.x * go.size.y;
-		var deleteCount = 0;
-		for(var x = 0; x<go.size.x;x++){
-			for(var y=0;y<go.size.y;y++){
-				var index = (topLeft.x+x) + 'x' + (topLeft.y+y); //String.format("{0}_{1}", topLeft.x+x, topLeft.y+y);
-				if(this.battlefield[index]!=undefined && this.battlefield[index][go.id] != undefined){
-					delete this.battlefield[index][go.id];
-					deleteCount++;
-					if(isEmpty(this.battlefield[index])){
-						delete this.battlefield[index];
-					}
-				}
-			}
-		}
-		if(deleteCount != controlSum)
-		{
-			console.log('removeGlobal called');
-			this.removeGlobal(go);
-		}
+		// var topLeft = new Vector2(Math.round(go.position.x - go.size.x/2), Math.round(go.position.y - go.size.y/2));
+		// var controlSum = go.size.x * go.size.y;
+		// var deleteCount = 0;
+		// for(var x = 0; x<go.size.x;x++){
+		// 	for(var y=0;y<go.size.y;y++){
+		// 		var index = (topLeft.x+x) + 'x' + (topLeft.y+y); //String.format("{0}_{1}", topLeft.x+x, topLeft.y+y);
+		// 		if(this.battlefield[index]!=undefined && this.battlefield[index][go.id] != undefined){
+		// 			delete this.battlefield[index][go.id];
+		// 			deleteCount++;
+		// 			if(isEmpty(this.battlefield[index])){
+		// 				delete this.battlefield[index];
+		// 			}
+		// 		}
+		// 	}
+		// }
+		// if(deleteCount != controlSum)
+		// {
+		// 	console.log('removeGlobal called');
+		// 	this.removeGlobal(go);
+		// }
 	},
 	removeGlobal: function(go){
 		//var topLeft = new Vector2(Math.round(go.position.x - go.size.x/2), Math.round(go.position.y - go.size.y/2));
